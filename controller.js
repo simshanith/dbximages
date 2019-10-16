0e785270cb621305a24567b095c8a78db30f5392
Shane Daniel <simrpgman@gmail.com>
2019-10-15 17:09:05 -0700

    Recursive solution for nested subfolders

--- controller.js
+++ controller.js
@@ -160,10 +160,10 @@ It is a two step process:
 async function getLinksAsync(token){
 
   //List images from the root of the app folder
-  let result= await listImagePathsAsync(token,'');
+  let paths= await listImagePathsAsync(token,'');
 
   //Get a temporary link for each of those paths returned
-  let temporaryLinkResults= await getTemporaryLinksForPathsAsync(token,result.paths);
+  let temporaryLinkResults= await getTemporaryLinksForPathsAsync(token,paths);
 
   //Construct a new array only with the link field
   var temporaryLinks = temporaryLinkResults.map(function (entry) {
@@ -174,39 +174,42 @@ async function getLinksAsync(token){
 }
 
 
-/*
-Returns an object containing an array with the path_lower of each 
-image file and if more files a cursor to continue */
+/* Returns an array with the path_lower of each image file */
 async function listImagePathsAsync(token,path){
+  const imageEntries = await getImagesInFolder(token, path)
+  return imageEntries.map(({ path_lower }) => path_lower)
+}
 
-  let options={
+async function getImagesInFolder(token, path) {
+  const options={
     url: config.DBX_API_DOMAIN + config.DBX_LIST_FOLDER_PATH, 
     headers:{"Authorization":"Bearer "+token},
     method: 'POST',
     json: true ,
     body: {"path":path}
   }
+  const result = await rp(options)
+  const folderEntries = getFolderEntries(result.entries)
+  const nested = await Promise.all(folderEntries.map(({ path_lower }) => {
+    return getImagesInFolder(token, path_lower)
+  }))
 
-  //Make request to Dropbox to get list of files
-  let result = await rp(options);
-
-  //Filter response to images only
-  let entriesFiltered= result.entries.filter(function(entry){
-    return entry.path_lower.search(/\.(gif|jpg|jpeg|tiff|png)$/i) > -1;
-  });
-
-  //Get an array from the entries with only the path_lower fields
-  var paths = entriesFiltered.map(function (entry) {
-    return entry.path_lower;
-  });
-
-  //return a cursor only if there are more files in the current folder
-  let response= {};
-  response.paths= paths;
-  if(result.hasmore) response.cursor= result.cursor;
-  return response;
+  nested.unshift(result.entries);
+  return nested.flatMap(getImageEntries)
 }
 
+
+function getFolderEntries(entries) {
+  return entries.filter(entry => {
+    return entry['.tag'] === 'folder'
+  })
+}
+function getImageEntries(entries) {
+  return entries.filter(entry => {
+    return entry['.tag'] === 'file' &&
+      entry.path_lower.search(/\.(gif|jpg|jpeg|tiff|png)$/i) > -1;
+  });
+}
 
 //Returns an array with temporary links from an array with file paths
 function getTemporaryLinksForPathsAsync(token,paths){
